7d09b506ff85033fd1f986466c901e81a77a2668
Shilpi kumari <70882055+shilpi711@users.noreply.github.com>
2022-08-03 22:32:13 +0530

    jira completed

--- src/main/java/com/example/myapp/controller/homecontroller.java
+++ src/main/java/com/example/myapp/controller/homecontroller.java
@@ -1,14 +1,14 @@
-package com.example.myprojectHMSController;
+package com.example.myapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class homecontroller {
-	
-	    @GetMapping("/hello")
-	    public String hello() {
-	        return "hello world";
+	@GetMapping("/hello")
+	public String helloApi()
+	{
+		return "hello spring rest API";
+	}
 
-}
 }
--- src/test/java/com/example/myapp/MyApp1ApplicationTests.java
+++ src/test/java/com/example/myapp/MyApp1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.myprojectHMS;
+package com.example.myapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyAppApplicationTests {
+class MyApp1ApplicationTests {
 
 	@Test
 	void contextLoads() {
